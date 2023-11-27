1afd85ef04514ff43c0b2b03950bb88966e2ccb9
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-11-27 19:13:41 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/fs-82-react-node-adoppp@9bbada1f021ed829c424d43866f2a3980cae6f3d 🚀

---
+++ assets/PrivateContainer-30c8cd6b.js
@@ -0,0 +1,1 @@
+import{c as t,j as s}from"./index-03e432fb.js";import{H as e}from"./Header-8be28ec4.js";const a="_container_1ws8g_1",o={container:a},r=t.bind(o),m=({children:n})=>s.jsxs("section",{children:[s.jsx(e,{}),s.jsx("div",{className:r("container"),children:n})]});export{m as default};
